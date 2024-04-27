c38c6494539d864dbd79db9e88275243aaa7a8ca
anjwsm <anjalikushwahawsm@gmail.com>
2024-04-28 01:07:50 +0530

    Explore menu section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ export const Header = () => {
        <div className='header-contents'>
           <h2> Order your favourite food here </h2>
           <p>Choose from a diverse menu featuring  a delectable array of dishes crafted with the finest ingredients and culinary experience that satisfies your cravings and elevate your dining experience , one delicious meal at a time  </p>
-
+           <button>View Menu</button>
        </div>
 
     </div>
