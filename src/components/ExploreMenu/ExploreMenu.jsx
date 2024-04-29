b52ca4ec9cc3826267538ccce79676548a99f0f6
anjwsm <anjalikushwahawsm@gmail.com>
2024-04-29 10:43:06 +0530

    ui fixed

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,13 +2,16 @@ import React from 'react' ;
 import './ExploreMenu.css' ;
 import { menu_list } from '../../assets/assets';
 
+
 export const ExploreMenu = ({category , setCategory}) => {
+  console.log(category , "category")
   return (
     <div className='explore-menu' id='explore-menu'>
     <h1>Explore our menu</h1>
     <p className='explore-menu-text'>Choose from a diverse menu featuring  a delectable array of dishes crafted with the finest ingredients and culinary experience that satisfies your cravings and elevate your dining experience , one delicious meal at a time </p>
     <div className='explore-menu-list'>
           {menu_list.map((itemm, index) => {
+            console.log(itemm)
             return (
                 <div onClick={() => setCategory(prev => prev===itemm.menu_name ?"All" : itemm.menu_name)} key={index} className='explore-menu-list-item' >
                    <img className={category===itemm.menu_name?"active" :"" } src={itemm.menu_image} alt='' />
